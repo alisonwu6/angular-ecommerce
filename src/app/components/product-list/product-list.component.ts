de580fba31a701814c5f2e2f09c65ae825edbb29
Alison Wu <alison.wu23@gmail.com>
2025-01-09 12:37:32 +1000

    feat(16): 07-pagination-select-page-size

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -20,7 +20,7 @@ export class ProductListComponent implements OnInit {
 
   // new properties for pagination
   thePageNumber: number = 1;
-  thePageSize: number = 50;
+  thePageSize: number = 5;
   theTotalElements: number = 0;
 
   constructor(private productService: ProductService,
@@ -96,5 +96,11 @@ export class ProductListComponent implements OnInit {
         this.thePageSize = data.page.size;
         this.theTotalElements = data.page.totalElements;
       });
+  }
+
+  updatePageSize(pageSize: string) {
+    this.thePageSize = +pageSize;    //  Converts the pageSize value to a number type
+    this.thePageNumber = 1;
+    this.listProducts();
   }
 }

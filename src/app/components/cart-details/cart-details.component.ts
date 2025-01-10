b5958e31065a854f39cc283762dc2132ecc68cfb
Alison Wu <alison.wu23@gmail.com>
2025-01-10 13:06:27 +1000

    feat(16): 16-shopping-cart-details-decrement-item-quantity-and-remove

--- src/app/components/cart-details/cart-details.component.ts
+++ src/app/components/cart-details/cart-details.component.ts
@@ -23,7 +23,6 @@ export class CartDetailsComponent implements OnInit {
   listCartDetails() {
     // get a handle to the cart items
     this.cartItems = this.cartService.cartItems;
-    
 
     // subscribe to the cart totalPrice
     this.cartService.totalPrice.subscribe((data) => (this.totalPrice = data));
@@ -39,5 +38,13 @@ export class CartDetailsComponent implements OnInit {
 
   incrementQuantity(theCartItem: CartItem) {
     this.cartService.addToCart(theCartItem);
+  }
+
+  decrementQuantity(theCartItem: CartItem) {
+    this.cartService.decrementQuantity(theCartItem);
+  }
+
+  remove(theCartItem: CartItem) {
+    this.cartService.remove(theCartItem);
   }
 }
